9aeef58009fc6f5db4af526bcdb9221ad054c68a
nukru <nu_kru@hotmail.com>
2014-10-10 15:58:56 +0200

    add async decorator

--- app/decorators.py
+++ app/decorators.py
@@ -1,5 +1,7 @@
 from functools import wraps
 from flask import abort
+from threading import Thread
+
 
 user_list = ["arevalo","dani","chob","chueca","edu","richi","julio","borja","javi"]
 
@@ -12,3 +14,9 @@ def valid_user(f):
             return abort(404)
     return decorated_function
     
+
+def async(f):
+    def wrapper(*args, **kwargs):
+        thr = Thread(target = f, args = args, kwargs = kwargs)
+        thr.start()
+    return wrapper
